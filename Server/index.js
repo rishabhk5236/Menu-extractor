e0ee6a23988a3a8e30464c5ec5d77f35f231e54c
rishabhk5236 <rishabhk5236@gmail.com>
2025-02-14 10:35:32 +0530

    Commit 1

---
+++ Server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.post("/scrape", async (req, res) => {
+  const { url } = req.body;
+  if (!url) return res.status(400).json({ error: "No URL provided" });
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--disable-http2"],
+    });
+    const page = await browser.newPage();
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    );
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+    });
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    let bodyText = await page.evaluate(() => document.body.innerText);
+
+    await browser.close();
+
+    res.json({ text: bodyText });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
