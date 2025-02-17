24ff3c141379710810a565046cf13af690dbb80e
rishabhk5236 <rishabhk5236@gmail.com>
2025-02-17 14:53:24 +0530

    Changed some commands in batch file

--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 @echo off
 
-cd .\server\
-start cmd /k "node --watch .\index.js" 
+:: Start the server
+cd /d "%~dp0server"
+start "Server" cmd /k "node --watch index.js"
+
+:: Wait a few seconds for the server to start
 timeout /t 3 /nobreak >nul
 
-
-start cmd /k 
-cd ..\
-cd .\client\
-npm run start
-
-pause
+:: Start the client
+cd /d "%~dp0client"
+start "Client" cmd /k "npm run start"
